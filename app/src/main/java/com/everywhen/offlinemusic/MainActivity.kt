326f20a517b4147e1d8724ccbac1d66cf1a769d3
judahqfish <judahqfish@gmail.com>
2026-09-14 15:03:30 +0300

    Add main activity

---
+++ app/src/main/java/com/everywhen/offlinemusic/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.everywhen.offlinemusic
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MaterialTheme(colorScheme = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme()) {
+                val vm: MainViewModel = viewModel()
+                MusicApp(vm)
+            }
+        }
+    }
+}
