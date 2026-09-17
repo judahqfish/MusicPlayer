fb582cfc4a1eed11c13710f2e3bed5e78d53a1e5
judahqfish <judahqfish@gmail.com>
2026-09-17 13:50:38 +0300

    Bump MusicPlayer to v0.1.35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.everywhen.offlinemusic"
         minSdk = 26
         targetSdk = 37
-        versionCode = 35
-        versionName = "0.1.34"
+        versionCode = 36
+        versionName = "0.1.35"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
